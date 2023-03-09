03b84dbd971e7ec5380320637e1c52c006172506
Léonie St-Onge <leonie.stonge@gmail.com>
2023-03-08 21:30:53 -0500

    Saving all the prefab into a Prefabs Folder
    Saving all prefabs, accordingly to instructions.
    Cleaning the hierarchie after.

--- PrefabManipulation/Assets/Editor/GeneratePrefabs.cs
+++ PrefabManipulation/Assets/Editor/GeneratePrefabs.cs
@@ -5,38 +5,66 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 using System;
+using System.Text.RegularExpressions;
 
 public static class GeneratePrefabs
 {
+
+    public static int nbOfGameObjects = 20;
+
     [MenuItem("Prefabs/Make Prefabs")]
     public static void Generate()
     {
-        //TODO : make a list of strings to mix and match
+        GameObject parent = new GameObject("MotherOfEnemies");
+        for (int i = 0; i < nbOfGameObjects; i++)
+        {
+            string name = GenerateRandomName();
+            GameObject go = new GameObject(name);
+            go.transform.SetParent(parent.transform);
 
+            //add rigidbody2D, spriteRenderer, BoxCollider2d
+            AddBasicComponents(go);
 
-        GameObject go = new GameObject("objectName");
+            //add the component script AI
+            AddAIComponent(go);
 
-        AddBasicComponents(go);
+            //randomly select a value for the enum in AI script
+            SelectaRandomEnumValue(go.GetComponent<Old_AIBase>());
 
-        AddAIComponent(go);
+            //link the refenrece to other components in the AI script component
+            LinkWithOtherComponents(go.GetComponent<Old_AIBase>(), go.GetComponent<Rigidbody2D>(), go.GetComponent<SpriteRenderer>(), go.GetComponent<BoxCollider2D>());
 
-        SelectaRandomEnumValue(go.GetComponent<Old_AIBase>());
+            //link to the AIStats scriptableObject
+            AddAIStatsReference(go.GetComponent<Old_AIBase>());
 
-        LinkWithOtherComponents(go.GetComponent<Old_AIBase>(), go.GetComponent<Rigidbody2D>(), go.GetComponent<SpriteRenderer>(), go.GetComponent<BoxCollider2D>());
+            //Save the prefab in the asset folder / Prefabs
+            SaveInAssetsFolder(go);
 
-        //Add a scriptable Assets (randomly)
-        //Give it a name : 
-        //by having a collection of words and mixing two words together
-        //Save the prefab to the project and refresh the asset database!!!!!!!!!!!!
+            //refresh the asset database!
+            AssetDatabase.Refresh();
+        }
+        ClearHierarchie(parent);
+    }
 
+    public static void SaveInAssetsFolder(GameObject go)
+    {
+        if (!Directory.Exists("Assets/Prefabs"))
+            AssetDatabase.CreateFolder("Assets", "Prefabs");
+        string localPath = "Assets/Prefabs/" + go.name + ".prefab";
+        bool prefabSuccess;
+        PrefabUtility.SaveAsPrefabAsset(go, localPath, out prefabSuccess);
+    }
 
+    public static void ClearHierarchie(GameObject parent)
+    {
+        int nbChild = parent.transform.childCount;
 
-        //if (!Directory.Exists("Assets/Prefabs"))
-        //    AssetDatabase.CreateFolder("Assets", "Prefabs");
-        //string localPath = "Assets/Prefabs/" + go.name + ".prefab";
+        for (int i = parent.transform.childCount - 1; i >= 0; i--)
+        {
+            GameObject.DestroyImmediate(parent.transform.GetChild(i).gameObject);
+        }
 
-        //Resources.Load<AIStats>() (precise that I want to LOAD a scriptable object!
-        //Resources.LoadAll<AIStats>()
+        GameObject.DestroyImmediate(parent.gameObject);
     }
 
     private static void AddBasicComponents(GameObject go)
@@ -50,12 +78,10 @@ public static class GeneratePrefabs
     {
         string fullPath = "Assets/Scripts/Old_Scripts/Old_AIBase_Children";
 
-
         string[] fileEntries = Directory.GetFiles(fullPath, "*.cs");
         for (int i = 0; i < fileEntries.Length; i++)
         {
             fileEntries[i] = Path.GetFileNameWithoutExtension(fileEntries[i]);
-            Debug.Log(fileEntries[i]);
         }
 
         int index = UnityEngine.Random.Range(0, fileEntries.Length - 1);
@@ -69,7 +95,7 @@ public static class GeneratePrefabs
     public static void SelectaRandomEnumValue(Old_AIBase component)
     {
         int range = System.Enum.GetValues(typeof(EnumReferences.EnemyType)).Length;
-        component.enemyType = (EnumReferences.EnemyType) UnityEngine.Random.Range(0, range);           
+        component.enemyType = (EnumReferences.EnemyType)UnityEngine.Random.Range(0, range);
     }
 
     public static void LinkWithOtherComponents(Old_AIBase component, Rigidbody2D rb, SpriteRenderer sr, BoxCollider2D bc)
@@ -77,5 +103,30 @@ public static class GeneratePrefabs
         component.rb = rb;
         component.sr = sr;
         component.coli = bc;
+    }
+
+    public static void AddAIStatsReference(Old_AIBase component)
+    {
+        //Add a scriptable Assets (randomly)
+        string path = "Scriptable Assets";
+        AIStats[] arr = Resources.LoadAll<AIStats>(path);
+        int index = UnityEngine.Random.Range(0, arr.Length - 1);
+        component.aiStats = arr[index];
+    }
+    public static string GenerateRandomName()
+    {
+        string variableName = "";
+        string folderPath = "Assets/Resources/Prefabs";
+
+        do
+        {
+            //Randomly make a new name starting with "gameObject_" and adding other alphanum to it.
+            variableName = "gameObject_" + Guid.NewGuid().ToString().Replace("-", "");
+            //removing all the numbers
+            variableName = Regex.Replace(variableName, @"[\d-]", string.Empty);
+            //making sure it is not already the name of an abject in the file
+        } while (AssetDatabase.FindAssets(Regex.Replace(variableName, @"[\d-]", string.Empty), new[] { folderPath }).Length > 0);
+
+        return variableName;
     }
 }
--- PrefabManipulation/Assets/Scripts/New_Scripts/AIBase.cs
+++ PrefabManipulation/Assets/Scripts/New_Scripts/AIBase.cs
@@ -29,7 +29,7 @@ public class AIBase : MonoBehaviour
             if(aiStats.hp < 0)
             {
                 GameObject.Destroy(gameObject);
-                //GameObject.DestroyImmediate ??
+                //GameObject.DestroyImmediate(gameObject, true);
             }
         }
 
