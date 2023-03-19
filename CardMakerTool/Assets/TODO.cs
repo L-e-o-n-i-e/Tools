a1009046afcbb5fc5e2d31559db03676500550fb
Léonie St-Onge <leonie.stonge@gmail.com>
2023-03-19 17:33:28 -0400

    TODO sheet updated.

--- CardMakerTool/Assets/TODO.cs
+++ CardMakerTool/Assets/TODO.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class TODO : MonoBehaviour
 {
     //Custom Window :
-        //Call OnInspectorGUI for the ScriptableObject and display it.
-        //Load button :  opens file explorer : select a scriptable object that will render in the window
+        
         //Display the data side to side at first
         //Buttons : previous and next : cycles trought file of scriptable objects (put them in an array % length)
         //Button : Go To File :  highlight the scriptableObject in Project window (below)
